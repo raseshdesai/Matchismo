eadc960dc8dcda3b086099e75d58d48f0cb28e7a
Rasesh Desai <rasesh.desai@gmail.com>
2013-04-20 14:40:44 -0500

    comiting via Xcode on iMac - just added new cardMatchingGame Model

---
+++ Matchismo/CardMatchingGame.h
@@ -0,0 +1,15 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Rasesh Desai on 4/20/13.
+//  Copyright (c) 2013 Rasesh Desai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatchingGame : NSObject
+
+
+
+@end
---
+++ Matchismo/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  Matchismo
+//
+//  Created by Rasesh Desai on 4/20/13.
+//  Copyright (c) 2013 Rasesh Desai. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
